632d7c32724d42ef3a3fbefafdbdfd88d5ce86d4
LeadingIndia.AI <43125408+LeadingIndiaAI@users.noreply.github.com>
2018-09-12 15:27:03 +0530

    Add files via upload

---
+++ Train.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 18 16:13:49 2018
+
+@author: leksh
+"""
+
---
+++ validate.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 20 09:42:56 2018
+
+@author: leksh
+"""
